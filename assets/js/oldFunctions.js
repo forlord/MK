13337309b6a907dcac127415367d6b08617770e5
RichrdYashiro <forlord@yandex.ru>
2021-04-20 19:06:02 +0300

    домашка
    так и не понял почему при rest теряется  this
    и как из одного модуля можно вызвать функцию другого

---
+++ assets/js/oldFunctions.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ const $randomButton = document.querySelector('.button');
+function gameOverOld(player) {
+    $randomButton.style.display = 'none';
+    const name = (player.player === 1) ? player2.name : player1.name;
+    $arenas.appendChild(playerWin(name));
+    createReloadButton();
+}
+/*
+ $randomButton.addEventListener('click', function () {
+ const player = (getRandom(1, 2) == 1) ? player1 : player2;
+ player.changeHP(getDamage(30));
+ player.renderHP();
+ if (player.hp === 0)
+ gameOver(this);
+ });*/
